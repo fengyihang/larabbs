029bb9a360eaa6fe844c4f6f955b98a0749720c6
grace <1070609282@qq.com>
2019-12-31 01:48:08 +0800

    活跃用户

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class TopicsController extends Controller
 {
@@ -17,11 +18,14 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
-	{
-		$topics = Topic::with('user', 'category')->paginate(15);
-		return view('topics.index', compact('topics'));
-	}
+    public function index(Request $request, Topic $topic, User $user)
+    {
+        $topics = $topic->withOrder($request->order)
+            ->with('user', 'category')  // 预加载防止 N+1 问题
+            ->paginate(20);
+        $active_users = $user->getActiveUsers();
+        return view('topics.index', compact('topics', 'active_users'));
+    }
 
     public function show(Request $request, Topic $topic)
     {
--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Str;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
+    use Traits\ActiveUserHelper;
     use HasRoles;
     use MustVerifyEmailTrait;
 
