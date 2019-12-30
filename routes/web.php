bef98b802d9cbb2cb9332307ae8258e801b018fe
grace <1070609282@qq.com>
2019-12-31 02:25:49 +0800

    首页话题列表

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 //Route::get('/home', 'HomeController@index')->name('home');
 
 //Auth::routes();
