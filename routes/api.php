9884b5832c98659ceb5e35ef580cadae59ef7bb2
grace <1070609282@qq.com>
2019-12-31 18:06:05 +0800

    发布话题

--- routes/api.php
+++ routes/api.php
@@ -18,27 +18,22 @@ Route::prefix('v1')->namespace('Api\v1')->name('api.v1.')->group(function() {
     Route::middleware('throttle:' . config('api.rate_limits.sign'))
         ->group(function () {
             // 图片验证码
-            Route::post('captchas', 'CaptchasController@store')
-                ->name('captchas.store');
+            Route::post('captchas', 'CaptchasController@store')->name('captchas.store');
             // 短信验证码
-            Route::post('verificationCodes', 'VerificationCodesController@store')
-                ->name('verificationCodes.store');
+            Route::post('verificationCodes', 'VerificationCodesController@store')->name('verificationCodes.store');
             // 用户注册
-            Route::post('users', 'UsersController@store')
-                ->name('users.store');
+            Route::post('users', 'UsersController@store')->name('users.store');
             // 第三方登录
             Route::post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
                 ->where('social_type', 'weixin')
                 ->name('socials.authorizations.store');
             // 登录
-            Route::post('authorizations', 'AuthorizationsController@store')
-                ->name('api.authorizations.store');
+            Route::post('authorizations', 'AuthorizationsController@store')->name('api.authorizations.store');
             // 刷新token
             Route::put('authorizations/current', 'AuthorizationsController@update')
                 ->name('authorizations.update');
             // 删除token
-            Route::delete('authorizations/current', 'AuthorizationsController@destroy')
-                ->name('authorizations.destroy');
+            Route::delete('authorizations/current', 'AuthorizationsController@destroy')->name('authorizations.destroy');
         });
 
     Route::middleware('throttle:' . config('api.rate_limits.access'))
@@ -46,24 +41,27 @@ Route::prefix('v1')->namespace('Api\v1')->name('api.v1.')->group(function() {
             // 游客可以访问的接口
 
             // 某个用户的详情
-            Route::get('users/{user}', 'UsersController@show')
-                ->name('users.show');
+            Route::get('users/{user}', 'UsersController@show')->name('users.show');
             // 分类列表
-            Route::get('categories', 'CategoriesController@index')
-                ->name('categories.index');
+            Route::get('categories', 'CategoriesController@index')->name('categories.index');
+            // 话题列表，详情
+            Route::resource('topics', 'TopicsController')->only([
+                'index', 'show'
+            ]);
 
 
             // 登录后可以访问的接口
             Route::middleware('auth:api')->group(function() {
                 // 当前登录用户信息
-                Route::get('user', 'UsersController@me')
-                    ->name('user.show');
+                Route::get('user', 'UsersController@me')->name('user.show');
                 // 编辑登录用户信息
-                Route::patch('user', 'UsersController@update')
-                    ->name('user.update');
+                Route::patch('user', 'UsersController@update')->name('user.update');
                 // 上传图片
-                Route::post('images', 'ImagesController@store')
-                    ->name('images.store');
+                Route::post('images', 'ImagesController@store')->name('images.store');
+                // 发布话题
+                Route::resource('topics', 'TopicsController')->only([
+                    'store', 'update', 'destroy'
+                ]);
             });
         });
 });
