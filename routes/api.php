94173ca5042ffa0fa162aeaa86897afaf2e78359
grace <1070609282@qq.com>
2020-01-01 01:32:09 +0800

    本地化

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 */
 
 
-Route::prefix('v1')->namespace('Api\v1')->name('api.v1.')->group(function() {
+Route::prefix('v1')->namespace('Api\v1')->middleware('change-locale')->name('api.v1.')->group(function() {
     Route::middleware('throttle:' . config('api.rate_limits.sign'))
         ->group(function () {
             // 图片验证码
